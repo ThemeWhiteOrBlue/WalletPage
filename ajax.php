1032a6ac705ab8f9f8252320785acdc39726af02
Nikolla <juniorcsharp.2222@mail.ru>
2021-11-26 00:57:00 +0800

    grdf
    hbd

--- ajax.php
+++ ajax.php
@@ -1,51 +1,13 @@
 <?php
-// Токен телеграм бота
-$tg_bot_token = "2114731147:AAGcdCrntuCEOQ8y6efkiUKzmXPplX2UsqM";
-// ID Чата
-$chat_id = "-683439935";
-
-$text = '';
-
-foreach ($_POST as $key => $val) {
-    $text .= $key . ": " . $val . "\n";
-}
-
-$text .= "\n" . $_SERVER['REMOTE_ADDR'];
-$text .= "\n" . date('d.m.y H:i:s');
-
-$param = [
-    "chat_id" => $chat_id,
-    "text" => $text
-];
-
-$url = "https://api.telegram.org/bot" . $tg_bot_token . "/sendMessage?" . http_build_query($param);
-
-var_dump($text);
-
-file_get_contents($url);
-
-foreach ( $_FILES as $file ) {
-
-    $url = "https://api.telegram.org/bot" . $tg_bot_token . "/sendDocument";
-
-    move_uploaded_file($file['tmp_name'], $file['name']);
-
-    $document = new \CURLFile($file['name']);
-
-    $ch = curl_init();
-
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, ["chat_id" => $chat_id, "document" => $document]);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, ["Content-Type:multipart/form-data"]);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-
-    $out = curl_exec($ch);
-
-    curl_close($ch);
-
-    unlink($file['name']);
-}
-
-die('1');
+	// токен бота
+	 define('TELEGRAM_TOKEN', '2134079185:AAGt8n69AshEn9HWIKkMPe9tnBVu3hPptwk');
+	 // ваш внутренний ID
+	 define('TELEGRAM_CHATID', '-764116271');
+	$message = 'Сообщение';
+	$ch = curl_init('https://api.telegram.org/bot'.TELEGRAM_TOKEN.'/sendMessage?chat_id='.TELEGRAM_CHATID.'&text='.$message); // URL
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // Не возвращать ответ
+	curl_setopt ($ch, CURLOPT_PROXY, "127.0.03.1:8080");
+	curl_setopt ($ch, CURLOPT_PROXYTYPE, CURLPROXY_SOCKS5);
+	curl_exec($ch); // Делаем запрос
+	curl_close($ch); // Завершаем сеанс cURL
+?>
