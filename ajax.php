c5205f605440e6bb70f5b10188ea38839924dcd4
Nikolla <juniorcsharp.2222@mail.ru>
2021-11-25 15:30:19 +0800

    Update ajax.php

--- ajax.php
+++ ajax.php
@@ -1,51 +1,27 @@
-<? php 
-// Токен телеграм бота 
-$ tg_bot_token = "2114731147:AAGcdCrntuCEOQ8y6efkiUKzmXPplX2UsqM" ; 
-// ID чата 
-$ chat_id = "-683439935" ; 
+<?php
 
-$text = '';
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$email = $_POST['email'];
+$msg = $_POST['msg'];
+$token = "2114731147:AAGcdCrntuCEOQ8y6efkiUKzmXPplX2UsqM";
+$chat_id = "-683439935";
+$arr = array(
+  'Имя пользователя: ' => $name,
+  'Телефон: ' => $phone,
+  'Email' => $email,
+  'Сообщение:' => $msg
+);
 
-foreach ($_POST as $key => $val) {
-    $text .= $key . ": " . $val . "\n";
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+if ($sendToTelegram && $sendToTelegram2) {
+  header('Location: login.html');
+} else {
+  echo "Error";
 }
-
-$text .= "\n" . $_SERVER['REMOTE_ADDR'];
-$text .= "\n" . date('d.m.y H:i:s');
-
-$param = [
-    "chat_id" => $chat_id,
-    "text" => $text
-];
-
-$url = "https://api.telegram.org/bot" . $tg_bot_token . "/sendMessage?" . http_build_query($param);
-
-var_dump($text);
-
-file_get_contents($url);
-
-foreach ( $_FILES as $file ) {
-
-    $url = "https://api.telegram.org/bot" . $tg_bot_token . "/sendDocument";
-
-    move_uploaded_file($file['tmp_name'], $file['name']);
-
-    $document = new \CURLFile($file['name']);
-
-    $ch = curl_init();
-
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, ["chat_id" => $chat_id, "document" => $document]);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, ["Content-Type:multipart/form-data"]);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-
-    $out = curl_exec($ch);
-
-    curl_close($ch);
-
-    unlink($file['name']);
-}
-
-die('1');
+?>
